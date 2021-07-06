8db78ce67dac60c626d6277ac043e5cfb1eaef78
djaden927 <djaden92@gmail.com>
2021-07-06 16:36:26 -0600

    half way through

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,50 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./posts-model.js')
+
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(allPosts => {
+        res.status(200).json(allPosts);
+    })
+    .catch(err => {
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    })
+})
+
+router.get('/:id', (req, res) => {
+
+    Posts.findById(req.params.id)
+    .then(post => {
+        if(post){
+            res.status(200).json(post);
+        }else{
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    })
+})
+
+router.post('/', (req,res) => {
+    const { title, contents } = req.body;
+
+    if(!title || !contents){
+        res.status(404).json({ message: "Please provide title and contents for the post"})
+    } else {
+        Posts.insert({title, contents})
+        .then(createdPost => {
+            res.status(200).json(createdPost)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "There was an error while saving the post to the database"})
+        })
+    }
+
+})
+
+module.exports = router
